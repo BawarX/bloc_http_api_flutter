542413629463b7480f30d780b040328f160a675b
bawar <bawarkhalid912@gmail.com>
2023-07-07 14:31:45 +0300

    added states to main body

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:bloc_http_api/blocs/bloc_class.dart';
 import 'package:bloc_http_api/blocs/bloc_event.dart';
+import 'package:bloc_http_api/blocs/bloc_state.dart';
 import 'package:bloc_http_api/repos/repository.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,6 +21,20 @@ class _homeState extends State<home> {
       create: (context) => userBloc(
         RepositoryProvider.of<userRepository>(context),
       )..add(LoadUserEvent()),
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text('The Bloc App'),
+          ),
+          body: BlocBuilder<userBloc, UserState>(
+            builder: (context, state) {
+              if (state is userLoadedState) {
+                return const Center(
+                  child: Text("data is Loading, please wait"),
+                );
+              }
+              return Container();
+            },
+          )),
     );
   }
 }
